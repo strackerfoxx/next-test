c09f8e2cfd8b7d0bdbd2a345b1a895c3565e4a01
strackerfoxx <86536635+strackerfoxx@users.noreply.github.com>
2023-03-04 00:01:42 -0600

    retroceso

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Layout from './Layout'
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Game({juego}) {
     const {
@@ -14,10 +12,10 @@ export default function Game({juego}) {
       minimum_system_requirements: minReq,
       publisher,
       release_date,
+      screenshots,
       short_description,
       status,
-      thumbnail,
-      platform
+      thumbnail
     } = juego
   return (
     <Layout>
@@ -25,31 +23,7 @@ export default function Game({juego}) {
           <title>{`${title} - Free-to-Play-Games`}</title> 
           <meta name="description" content={`Gaming Games free to play free games - ${short_description}`} /> 
         </Head>
-        <div className='contenedor producto full'>
-            <Image width={1000}  height={1000} src={thumbnail} alt={`imagen de: ${title}`} className='imagen'/>
-            <div>
-               <h1 className='heading title'>{title}</h1>
-               <h2 className='heading'>Genero: {genre}</h2>
-               <h2 className='heading'>Desarrollado por: {developer}</h2>
-                  <Link href={url}><button  className='verJuego'>Descargar Juego</button></Link>
-            </div>
-         </div>
-         <div className='descripcion'>
-            <h4>{description}</h4>
-         </div>
-         <div className='requisitos'>
-            {minReq.graphics && (
-              <div>
-                  <h2>Requisitos Minimos</h2>
-                  <h4>graficos: <span className='requisito'>{minReq.graphics}</span></h4>
-                  <h4>memoria: <span className='requisito'>{minReq.memory}</span></h4>
-                  <h4>os: <span className='requisito'>{minReq.os}</span></h4>
-                  <h4>procesador: <span className='requisito'>{minReq.processor}</span></h4>
-                  <h4>almacenamiento: <span className='requisito'>{minReq.storage}</span></h4>
-              </div>
-            )}
-         </div>
-        
+        <h3>{title}</h3>
     </Layout>
   )
 }
