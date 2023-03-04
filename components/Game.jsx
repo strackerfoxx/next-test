d69ca93d1cd065528de097068daf4b4365cb8d4d
strackerfoxx <86536635+strackerfoxx@users.noreply.github.com>
2023-03-04 00:07:42 -0600

    testV1

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from './Layout'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Game({juego}) {
     const {
@@ -21,9 +23,18 @@ export default function Game({juego}) {
     <Layout>
         <Head>
           <title>{`${title} - Free-to-Play-Games`}</title> 
-          <meta name="description" content={`Gaming Games free to play free games - ${short_description}`} /> 
+          <meta name="description" content={`${short_description}`} /> 
         </Head>
-        <h3>{title}</h3>
+        <div className='contenedor producto full'>
+            <Image width={1000}  height={1000} src={thumbnail} alt={`imagen de: ${title}`} className='imagen'/>
+            <div>
+               <h1 className='heading title'>{title}</h1>
+               <h2 className='heading'>Genero: {genre}</h2>
+               <h2 className='heading'>Desarrollado por: {developer}</h2>
+                  <Link href={url}><button  className='verJuego'>Descargar Juego</button></Link>
+            </div>
+         </div>
+         
     </Layout>
   )
 }
